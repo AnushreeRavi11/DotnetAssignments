93a929b70f83fd4ef411fcc1a736c25c9e0c1893
AnushreeRavi11 <114803810+AnushreeRavi11@users.noreply.github.com>
2022-10-06 19:32:03 +0530

    Assingment_1_And_Assingment_2

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        int a, c, b,num1,num2,k,T,m;
+        
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+             p.add();
+             
+            p.Postive();
+            
+            p.Compare();
+            Console.ReadLine();
+        }
+        public void Postive()
+        {
+
+            Console.WriteLine("enter num1 value ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num2 value");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > 0)
+
+                Console.WriteLine(" Yes the given number is postive ");
+
+            else
+                Console.WriteLine("the number is negative");
+
+        }
+        public void Compare()
+        {
+            Console.WriteLine("Enter k value ");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter m value ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" enter your choice ");
+            Console.WriteLine("1 Addition of 2 number");
+            Console.WriteLine("2 Subsrtraction");
+            Console.WriteLine("3 Division");
+            Console.WriteLine("4 multiplication");
+            
+             c=Convert.ToInt32(Console.ReadLine());
+
+            
+            switch (c)
+            {
+                case 1:
+                    T = k + m;
+                    Console.WriteLine("Addition of 2 number "+T);
+                    break;
+                case 2:
+                    T = k - m;
+                    Console.WriteLine("Substraction of 2 number "+T);
+                    break;
+                case 3:
+                    T = k * m;
+                        Console.WriteLine(  "Multiplication of 2 number "+T);
+                    break;
+                case 4:
+                    T = k / m;
+                    Console.WriteLine("Division of 2 numbers"+T);
+                    break;
+                default:
+                    Console.WriteLine(" entered wrong number  " + T);
+                    break;
+                    
+
+            }
+
+
+
+           
+           
+        }
+
+
+            public void add()
+        {
+
+            Console.WriteLine("enter a value ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter b value");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a == b)
+
+                Console.WriteLine("yes ");
+
+            else
+
+                Console.WriteLine("No");
+
+        }
+
+        }
+
+    }
+
+
+
+
+
+
+
---
+++ Day2Ass/Day2Ass/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Ass
+{
+    class Program
+    { 
+        public void three()
+        {
+            int[] arr = new int[5] { 3, 5, 8, 9,4 };
+            Console.WriteLine("The length od Arrya is{0}----->", arr.Length);
+            Console.WriteLine("reverse the given array{0}",    arr.Reverse());
+            
+
+            
+
+
+
+          }
+
+        public void  call()
+        {
+            int[] num = new int[4] { 5,9,8,10 };
+
+            Console.WriteLine("**************************************");
+            Console.WriteLine("Total Value of a Integer is {0}", num.Sum());
+            Console.WriteLine("maxmum value of a integer is {0}", num.Max());
+            Console.WriteLine("Avarage value of a intger is {0}", num.Average());
+            Console.WriteLine(  "Minimum value of Integer is {0}",num.Min());
+            Console.WriteLine("Descending value of Integer is {0}");
+            Array.Reverse(num);
+
+              
+
+        
+        }
+        public void calls()
+        {
+            int[] arr = new int[] { 10, 20, 30, 40 };
+
+            Console.WriteLine("minimum value of a integer is {0}", arr.Max());
+            Console.WriteLine("maxmum value of a integer is {0}", arr.Min());
+            Console.WriteLine("Avarage value of a intger is {0}", arr.Average());
+            Console.ReadLine();
+
+
+        }
+        static void Main(string[] args)
+
+        {
+            
+            Program p = new Program();
+            p.call();
+            p.calls();
+            p.three();
+            
+            Console.ReadLine();
+           
+
+
+        }
+
+
+
+
+
+    }
+}
