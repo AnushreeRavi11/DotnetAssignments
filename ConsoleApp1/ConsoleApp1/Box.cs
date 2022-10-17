fcf894030b6c05b3742c904eb1fb778d9491834d
AnushreeRavi11 <114803810+AnushreeRavi11@users.noreply.github.com>
2022-10-17 10:25:26 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Box
+    {
+        int length;
+        int breadth;
+
+        public int Adds(int a, int b)
+        {
+            int result;
+            result = length + breadth;
+            return result;
+
+
+        }
+        public static void Main(String[] args)
+        {
+            Box b2 = new Box();
+            
+            Console.WriteLine("Enter length");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter breadth");
+            int b = Convert.ToInt32(Console.ReadLine());
+            b.Adds(6,7);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Cricket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Cricket
+    {
+
+
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter No of matches: ");
+            int matches = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
